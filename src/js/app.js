529218a1c9e5afd3a2324f837941fae706701efd
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-26 11:21:18 +0300

    articles

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  for(let i = 0; i < 5; i++){
+    let createArticle = document.createElement('article');
+    createArticle.className = 'message';
+    createArticle.textContent = 'asd';
+    document.querySelector('body').appendChild(createArticle);
+  }
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
